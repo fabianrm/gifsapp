4629e62065e80f691437e958c9d6f80b785a2aee
Fabian Ramirez <fabianrm@gmail.com>
2024-10-13 20:11:36 -0500

    Carga de imagen desde el servicio y almacenamiento en localstorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     GifsModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,23 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, GifResponse } from '../interfaces/gifs.interfaces';
+
+//const GIPHY_API_KEY = 'rgwQ3Aty8iLeTV1Pe9U20pA2KCyReeFd';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private _tagsHistory: string[] = [];
+  gifList: Gif[] = [];
 
-  constructor() { }
+  private _tagsHistory: string[] = [];
+  private _apiKey: string = 'rgwQ3Aty8iLeTV1Pe9U20pA2KCyReeFd';
+  private _serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient,) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory]; //Getter que retorna una copia
@@ -21,12 +31,41 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag); //agrega a la primera posicion del array
     this._tagsHistory = this._tagsHistory.splice(0, 10); //recortamos a solo 10 elementos
+    this.saveLocalStorage();
   }
+
+
+  //Guardar en LocalStorage
+  private saveLocalStorage() {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  //Cargar LocalStorage
+  private loadLocalStorage() {
+    const tempTags = localStorage.getItem('history');
+
+    if (!tempTags) return;
+    this._tagsHistory = JSON.parse(tempTags)
+
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0])
+  }
+
 
   //Realiza la búsqueda
   searchTag(tag: string): void {
     if (!tag) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this._apiKey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get<GifResponse>(`${this._serviceUrl}/search?`, { params })
+      .subscribe(resp => {
+        this.gifList = resp.data;
+      })
   }
 
 
